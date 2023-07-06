39deab0c3d9b73300f4b7ef29b673cc18d6f8247
Kurage <shimeon.genko@gmail.com>
2023-07-06 03:20:59 +0200

    Update add simple response to on_message hello

--- warden.py
+++ warden.py
@@ -1,11 +1,13 @@
 import discord
 import os
+import random
+import asyncio
+import re
 from dotenv import load_dotenv
 
 load_dotenv() #load dotenv variables
 discord_token = os.getenv('DISCORD_TOKEN')
 discord_guild = os.getenv('DISCORD_GUILD')
-
 class MyClient(discord.Client):
     def __init__(self, intents):
         super().__init__(intents=intents)
@@ -13,8 +15,14 @@ class MyClient(discord.Client):
         print(f"Logged in as { self.user.name } (ID:{ self.user.id })")
         print(f"discord.py API version: { discord.__version__ }")
         print(f"------------------------------------------------")
-        print(f"Setting {self.user.name}'s status to { 'do not disturb','r' }")
+        print(f"Setting {self.user.name}'s status to { 'do not disturb' }")
         await self.change_presence(status=discord.Status.dnd)
+    async def on_message(self, message):
+        if message.author.id == self.user.id: # we do not want the bot to reply to itself
+            return
+        if message.content.startswith('!hello'):
+            await message.channel.send(f'Hello, {message.author.mention}. I, the {self.user.mention} bot says hi!')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
